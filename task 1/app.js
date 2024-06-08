5a4724df8d4389d228cbbf538db47ee2948c430e
AnasUsman70 <159641335+AnasUsman70@users.noreply.github.com>
2024-06-08 14:24:53 +0500

    Add files via upload

---
+++ task 1/app.js
@@ -0,0 +1,28 @@
+let exp = "faraz" || (true + false) + 5 && " " || "true";
+console.log(`0)${exp}`)
+let b = 3;
+let exp1 = (1 + 2) + ++b || 5 && 0 ; 
+console.log(`1)${exp1}`)
+let a = 5
+let exp2 = 32 && true - ++a && " " || "true"; 
+console.log(`2)${exp2}`)
+let exp3 = (true + 3) * 5 && undefined || "faraz" + 5;
+console.log(`3)${exp3}`)
+let exp4 = 32 + "abc" || (true + false) + false || + 5 || "true";
+console.log(`4)${exp4}`)
+let exp5 = false || (true + true) + 15 && "" || 50;
+console.log(`5)${exp5}`)
+let exp6 = (true + false) + 5 || false + 8 + "abc" || "true";
+console.log(`6)${exp6}`)
+let exp7 = "faraz" || false + 10 || "true";
+console.log(`7)${exp7}`)
+let exp8 = 12 + (false + false) + true && null || "faraz";
+console.log(`8)${exp8}`)
+let exp9 = (false + true + false + 2) || "faraz" && false || 1 + 10;
+console.log(`9)${exp9}`)
+let exp10 = (true + false) + 5 || false + 8 + "abc" || "true";
+console.log(`10)${exp10}`)
+let exp11 = (true + false) * 3 && "" || false + true - (12 + true + true + false);
+console.log(`11)${exp11}`)
+
+
